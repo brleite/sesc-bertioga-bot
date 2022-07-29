3a0678c95c93c343f89854dadd007b1d0ddab886
Bruno Mayerle Leite <brleite@gmail.com>
2022-07-29 15:51:25 -0300

    ajuste no script de execução no linux

--- scripts/run_sesc_bertioga_bot.sh
+++ scripts/run_sesc_bertioga_bot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+BASEDIR=$(dirname "$0")
 
-cd /home/brleite/projetos/SescBertiogaBot/
+cd $BASEDIR/../
 export NODE_HOME=/opt/node14
 export PATH=$PATH:$NODE_HOME/bin
 export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
